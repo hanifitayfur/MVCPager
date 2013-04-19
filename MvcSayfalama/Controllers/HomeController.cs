84449d0a42054c31c0042e1b2f9b2280e121284b
hanifitayfur <hanifitayfur@gmail.com>
2013-04-19 09:20:26 +0300

    Pager with jquery

---
+++ MvcSayfalama/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcSayfalama.Models;
+
+namespace MvcSayfalama.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Biletler = BiletGetir();
+            return View();
+        }
+        //ındex içerisinde gelecek olan datayı elde ediyoruz .İndex içinde döngüde   ViewBag.Biletler şeklinde ulaşırız
+        public List<Bilet> BiletGetir()
+        {
+            List<Bilet> list = new List<Bilet>();
+
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now)); 
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+            list.Add(new Bilet("İzmir Konseri", "PSY", DateTime.Now));
+            list.Add(new Bilet("Efes Konseri", "İsmail Yk", DateTime.Now));
+
+            return list;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcSayfalama/Models/Bilet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcSayfalama.Models
+{
+    public class Bilet
+    {
+        public string KonserAdi { get; set; }
+        public string Sarkici { get; set; }
+        public DateTime? DateAdded { get; set; }
+
+        public Bilet(string konser, string sarki,DateTime? dateadded)
+        {
+            KonserAdi = konser;
+            Sarkici = sarki;
+            DateAdded = dateadded;
+        }
+    }
+}
